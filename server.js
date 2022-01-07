e77b35f7c5f94ebc2ef00f61c8c24eb0051ad77d
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-01-06 19:40:17 -0500

    added routes for index, notes and a route for files that do no exist

---
+++ server.js
@@ -0,0 +1,33 @@
+// importing package express
+const express = require('express');
+
+// add app to express so we can chain it from now on
+const app = express();
+
+const path = require('path');
+
+app.use(express.static('public'));
+
+
+
+// sending index file
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+
+
+// start a server
+
+app.listen(3001, () => {
+    console.log("Server Started, listening on port 3001");
+})
