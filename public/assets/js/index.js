1e88e9354ad5a024ae5ab1cdbee37384c661510d
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-01-09 21:08:45 -0500

    completed the note app

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -68,6 +68,9 @@ const renderActiveNote = () => {
 
 const handleNoteSave = () => {
     const newNote = {
+        id: Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1),
         title: noteTitle.value,
         text: noteText.value,
     };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 const noteData = require('./db/db.json');
 const uid = require('./helpers/uuid.js');
-
+const fs = require('fs');
 
 
 app.use(express.static('public'));
@@ -25,22 +25,51 @@ app.get('/notes', function(req, res) {
 })
 
 app.get('/api/notes', (req, res) => {
+
     res.json(noteData)
 })
 
 app.post('/api/notes', (req, res) => {
-    dataRes = res.body;
-    const { title, text } = res.body;
-    res.send(title, text);
+    dataRes = req.body;
+    const { title, text } = req.body;
+    noteData.push(dataRes);
+    console.log(noteData)
+    fs.writeFile('./db/db.json', JSON.stringify(noteData),
+        (err) => {
+            if (err) {
+
+            } else {
+                res.status(200).send("Data saved!");
+            }
+        });
+
 
 })
 
+app.delete("/api/notes/:id", (req, res) => {
+    const { id } = req.params
+
+    console.log(id)
+    console.log(noteData)
+    noteData.splice(noteData.findIndex(data => data.id === id), 1)
+    console.log(noteData)
+    fs.writeFile('./db/db.json', JSON.stringify(noteData),
+        (err) => {
+            if (err) {
+
+            } else {
+                res.status(200).send(noteData);
+            }
+        });
+
+
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-
+app.delete('/api/notes')
 
 // start a server
 
